714ad4f332117854cf11397a5c6daa6dc659f0b6
George Grishchenkov <goscha-grishenkov@yandex.ru>
2023-12-15 01:34:14 +0300

    removed unnecessary code

--- redis/src/main/java/ratelimiter/RateLimiter.java
+++ redis/src/main/java/ratelimiter/RateLimiter.java
@@ -14,57 +14,20 @@ public class RateLimiter {
   private final String label;
   private final long maxRequestCount;
   private final long timeWindowSeconds;
-  private String START = "start";
-  private String CURR = "curr";
-  private String PREV = "prev";
-
-  private long limmiterStart;
-  private long boxNumber;
 
   public RateLimiter(Jedis redis, String label, long maxRequestCount, long timeWindowSeconds) {
     this.redis = redis;
     this.label = label;
     this.maxRequestCount = maxRequestCount;
     this.timeWindowSeconds = timeWindowSeconds;
-
-    START = "start" + label;
-    CURR = "curr" + label;
-    PREV = "prev" + label;
-
-    redis.set("0", "0");
-    redis.set("1", "0");
-    redis.set("-1", "0");
+    redis.flushAll();
   }
 
   public boolean pass() {
-    System.out.println();
-    if (limmiterStart == 0) {
-      limmiterStart = System.currentTimeMillis();
-    }
     long time = System.currentTimeMillis();
-    boxNumber = (time - limmiterStart) / 1000 / timeWindowSeconds;
-    redis.incr(""+boxNumber);
-
-    System.out.println("from start " + ""+(time - limmiterStart));
-    System.out.println("box number " + boxNumber);
-    System.out.println("now " + time);
-    System.out.println("max " + (time - timeWindowSeconds * 1000));
-
-    System.out.println("from box start " + (time - (limmiterStart + boxNumber*timeWindowSeconds*1000)));
-
     redis.zadd(label, (double) time, ""+time);
     redis.zremrangeByScore(label, 0, time - timeWindowSeconds * 1000);
-
-    System.out.println(redis.zrangeByScore(label, 0, time));
-    long request_count = redis.zcard(label);
-
-    System.out.println("req " + request_count);
-    if (request_count <= maxRequestCount) {
-      System.out.println(true);
-      return true;
-    }
-    System.out.println(false);
-    return false;
+    return redis.zcard(label) <= maxRequestCount;
   }
 
   public static void main(String[] args) {
